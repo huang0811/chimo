1cc24f50db5b35358ddf3f7a5643495f66f14bb9
huang0811 <s1100393@gm.pu.edu.tw>
2022-12-03 23:09:20 +0800

    make it better

--- index.py
+++ index.py
@@ -7,15 +7,68 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-from flask import Flask, render_template, request, make_response, jsonify
+from flask import Flask, render_template, request
 from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>黃鈺慈的vercel</h1>"
+    homepage = "<h1>育慈電影</h1>"
+    homepage += "<br><a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
+    homepage += "<br><a href=/search>電影查詢</a><br>"
     homepage += "<a href=/webhook>webhook</a><br>"
     return homepage
+	
+@app.route("/movie")
+def movie():
+    url = "http://www.atmovies.com.tw/movie/next/"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".filmListAllX li")
+    lastUpdate = sp.find("div", class_="smaller09").text[5:]
+
+    for item in result:
+        picture = item.find("img").get("src").replace(" ", "")
+        title = item.find("div", class_="filmtitle").text
+        movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+        hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
+        show = item.find("div", class_="runtime").text.replace("上映日期：", "")
+        show = show.replace("片長：", "")
+        show = show.replace("分", "")
+        showDate = show[0:10]
+        showLength = show[13:]
+
+        doc = {
+            "title": title,
+            "picture": picture,
+            "hyperlink": hyperlink,
+            "showDate": showDate,
+            "showLength": showLength,
+            "lastUpdate": lastUpdate
+         }
+
+        doc_ref = db.collection("電影").document(movie_id)
+        doc_ref.set(doc)
+    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate 
+
+@app.route("/search", methods=["POST","GET"])
+def search():
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        result = "請輸入您要查詢的課程關鍵字："+ MovieTitle
+        db = firestore.client()
+        collection_ref = db.collection("鈺慈電影")
+        docs = collection_ref.get()
+        info = ""
+        for doc in docs:
+            if MovieTitle in doc.to_dict()["title"]: 
+                info += "片名：" + doc.to_dict()["title"] + "<br>" 
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "分級資訊：" + doc.to_dict()["rate"] + "<br><br>"           
+        return info
+    else:  
+        return render_template("input.html")
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
